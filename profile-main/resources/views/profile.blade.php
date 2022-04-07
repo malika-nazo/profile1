853c957c6fd463141aa693150c32bb5648ebcdff
niaz <nyaz.797179@spu.edu.iq>
2022-04-07 04:38:13 -0800

    first commit

---
+++ profile-main/resources/views/profile.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <img src="picture.jpg" alt="niaz" width="512" height="341.5">
+    <p>My name is Niaz Hiwa, I am a college student</p>
+    <p>I work parttime in shar hospital</p>
+    <p>I am also makeup aritist used to work at salon</p>
+    <p>email: nyaz.797179@spu.edu.iq</p>
+</body>
+</html>
